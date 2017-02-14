e1ceb09ad474de8bbf8a0b9ec9bc57437c013181
Chris Townsend <ctownsen357@gmail.com>
2017-02-14 07:30:04 -0500

    Formatted with gofmt

--- main.go
+++ main.go
@@ -1,27 +1,27 @@
-package main
+ckage main
 
 import (
-    "github.com/nathan-osman/go-rpigpio"
-    "time"
-    "flag"
+        "flag"
+        "github.com/nathan-osman/go-rpigpio"
+        "time"
 )
 
 func main() {
-    //simple test to blink an LED n times on pin_num as specified by command line args
-    pin_num := flag.Int("pin_num", 17, "an int")
-    times := flag.Int("times", 10, "an int")
-    flag.Parse()
+        //simple test to blink an LED n times on pin_num as specified by command line args
+        pin_num := flag.Int("pin_num", 17, "an int")
+        times := flag.Int("times", 10, "an int")
+        flag.Parse()
 
-    p, err := rpi.OpenPin(*pin_num, rpi.OUT)
-    if err != nil {
-        panic(err)
-    }
-    defer p.Close()
+        p, err := rpi.OpenPin(*pin_num, rpi.OUT)
+        if err != nil {
+                panic(err)
+        }
+        defer p.Close()
 
-    for i := 0; i < *times; i++ {
-        p.Write(rpi.HIGH)
-        time.Sleep(300 * time.Millisecond)
-        p.Write(rpi.LOW)
-        time.Sleep(100 * time.Millisecond)
-    }
+        for i := 0; i < *times; i++ {
+                p.Write(rpi.HIGH)
+                time.Sleep(300 * time.Millisecond)
+                p.Write(rpi.LOW)
+                time.Sleep(100 * time.Millisecond)
+        }
 }
